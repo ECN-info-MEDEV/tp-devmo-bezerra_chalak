c0d693b14ef3bcba99690a3f4e13f91c62b1fdb8
Moufid CHALAK <144018760+mchalak2023@users.noreply.github.com>
2024-03-15 16:42:34 +0100

    changes

--- app/src/main/java/com/example/projetmedev/MainActivity.kt
+++ app/src/main/java/com/example/projetmedev/MainActivity.kt
@@ -4,13 +4,17 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextStyle
@@ -32,7 +36,8 @@ class MainActivity : ComponentActivity() {
                 ) {
                     Title(modifier= Modifier
                         .background(Color.Blue.copy(alpha = 0.5f))
-                        .padding(start = 16.dp, end = 226.dp, top = 16.dp, bottom = 16.dp))
+                        .padding(start = 16.dp, end = 166.dp, top = 16.dp, bottom = 16.dp))
+                    CompteCreation(modifier=Modifier.padding(top = 16.dp, bottom = 16.dp))
                 }
             }
         }
@@ -48,10 +53,21 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 }
 
 @Composable
+fun CompteCreation(modifier: Modifier = Modifier) {
+        Text(
+            text = "Créer un compte",
+            modifier = modifier,
+            color=Color.Yellow,
+            style = TextStyle(fontWeight = FontWeight.Bold),
+            textAlign = TextAlign.Center
+        )
+}
+
+@Composable
 fun Title(modifier: Modifier = Modifier) {
     Box {
         Text(
-            text = "Lien et bien-être hospitalier",
+            text = "Lien et bien-être en milieu hospitalier",
             modifier = modifier,
             style = TextStyle(fontWeight = FontWeight.Bold),
             textAlign = TextAlign.Center
@@ -63,6 +79,12 @@ fun Title(modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     ProjetMedevTheme {
-        Greeting("Android")
+        Column(verticalArrangement = Arrangement.Center) {
+            Title(modifier= Modifier
+                .background(Color.Blue.copy(alpha = 0.5f))
+                .padding(start = 16.dp, end = 166.dp, top = 16.dp, bottom = 16.dp))
+            CompteCreation(modifier=Modifier.padding(top = 16.dp, bottom = 16.dp))
+        }
+
     }
 }
--- app/src/main/java/com/example/projetmedev/ui/theme/Color.kt
+++ app/src/main/java/com/example/projetmedev/ui/theme/Color.kt
@@ -9,3 +9,9 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+val Bleue = Color(0xDEE9F5)
+object ColorPalette {
+    val Bleue = Color(0xDEE9F5)
+    val Red = Color(0xFF0000)
+    // Ajoutez d'autres couleurs ici si nécessaire
+}
