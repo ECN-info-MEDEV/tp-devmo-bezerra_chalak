b54ea23060b4158a1a4e97df4ff9f2b337886ffc
Dariel Bezerra <darielbezerra13@gmail.com>
2024-03-21 08:49:53 +0100

    modifs

--- app/src/main/java/com/example/projetmedev/JoaForCareScreen.kt
+++ app/src/main/java/com/example/projetmedev/JoaForCareScreen.kt
@@ -1,2 +1,8 @@
 package com.example.projetmedev
 
+import androidx.annotation.StringRes
+
+enum class CupcakeScreen(@StringRes val title: Int) {
+    Login(title = R.string.login),
+    Home(title = R.string.home)
+}
