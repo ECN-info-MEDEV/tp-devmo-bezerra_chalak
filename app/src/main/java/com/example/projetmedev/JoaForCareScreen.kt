cbb6b96b6c87b4de64ebcf0db4a36ba2945e991b
Dariel Bezerra <darielbezerra13@gmail.com>
2024-03-21 11:09:06 +0100

    navigation

--- app/src/main/java/com/example/projetmedev/JoaForCareScreen.kt
+++ app/src/main/java/com/example/projetmedev/JoaForCareScreen.kt
@@ -1,8 +1,12 @@
 package com.example.projetmedev
 
 import androidx.annotation.StringRes
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
@@ -16,16 +20,22 @@ import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
+import com.example.projetmedev.ui.theme.ColorPalette
 
 
 @Composable
@@ -45,9 +55,21 @@ fun JoaForCareApp(
             modifier = Modifier.padding(innerPadding)
         ) {
             composable(route = JoaForCareScreen.Login.name) {
+                var emailInput by remember { mutableStateOf("") }
+                var passwordInput by remember { mutableStateOf("") }
+                Column(verticalArrangement = Arrangement.Center, horizontalAlignment = Alignment.CenterHorizontally) {
+                    LoginScreen( email = emailInput,
+                        onValueChange = {passwordInput = it} ,
+                        password = passwordInput,
+                        onNextButtonClicked = { navController.navigate(JoaForCareScreen.Home.name) }
+                        ,modifier= Modifier
+                            .padding(top = 16.dp, bottom = 16.dp)
+                            .align(Alignment.CenterHorizontally))
+                }
 
             }
             composable(route = JoaForCareScreen.Home.name) {
+
             }
 
         }
--- app/src/main/java/com/example/projetmedev/LoginScreen.kt
+++ app/src/main/java/com/example/projetmedev/LoginScreen.kt
@@ -1,2 +1,153 @@
+
 package com.example.projetmedev
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Checkbox
+import androidx.compose.material3.CheckboxDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.OutlinedTextFieldDefaults
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.TextUnit
+import androidx.compose.ui.unit.TextUnitType
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.projetmedev.ui.theme.ColorPalette
+
+@Composable
+
+fun LoginScreen(modifier: Modifier = Modifier,
+                email: String,
+                onNextButtonClicked: () -> Unit = {},
+                onValueChange: (String) -> Unit,
+                password: String){
+    Image(
+        modifier=modifier,
+        painter = painterResource(id = R.drawable.logom),
+        contentDescription = null,
+        contentScale = ContentScale.Crop,
+        alpha = 0.5F
+    )
+
+    Box(
+        modifier = Modifier
+            .fillMaxWidth() // Define a largura máxima do Box
+            .height(40.dp) // Define a altura do Box como 100dp
+            .background(ColorPalette.Bleue), // Define a cor de fundo como azul
+        contentAlignment = Alignment.Center // Alinha o conteúdo ao centro vertical e horizontalmente
+    ) {
+        Text(
+            text = "Lien et bien-être en milieu hospitalier",
+            style = TextStyle(fontWeight = FontWeight.Bold),
+            textAlign = TextAlign.Center,
+            modifier = Modifier.padding(horizontal = 16.dp) // Adiciona padding horizontal para espaçamento
+        )
+    }
+    Text(
+        text = "Créer un compte",
+        modifier = modifier,
+        color= ColorPalette.Jaune,
+        style = TextStyle(fontWeight = FontWeight.Bold),
+        textAlign = TextAlign.Center
+    )
+    Text(
+        text = "Votre adresse mail",
+        modifier = modifier,
+        style = TextStyle(fontWeight = FontWeight.Bold),
+        textAlign = TextAlign.Center
+    )
+    OutlinedTextField(
+        value = email,
+        onValueChange = onValueChange,
+        singleLine = true,
+        shape = MaterialTheme.shapes.large,
+        label = { Text(text= "Digitez votre mail") },
+        colors = OutlinedTextFieldDefaults.colors(
+            focusedContainerColor = MaterialTheme.colorScheme.surface,
+            unfocusedContainerColor = MaterialTheme.colorScheme.surface,
+            disabledContainerColor = MaterialTheme.colorScheme.surface,
+            focusedBorderColor = ColorPalette.Jaune,
+            unfocusedBorderColor = ColorPalette.Jaune,
+        )
+    )
+    Column(verticalArrangement = Arrangement.Center, horizontalAlignment = Alignment.CenterHorizontally){
+        Text(
+            text = "Créez un mot de passe",
+            modifier = modifier,
+            style = TextStyle(fontWeight = FontWeight.Bold),
+            textAlign = TextAlign.Center
+        )
+        Text(
+            text = "(Le mot de pass doit contenir au minimum 8 caractères)",
+            modifier = modifier,
+            textAlign = TextAlign.Center,
+            style= TextStyle(fontSize = TextUnit(12f, TextUnitType.Sp))
+        )
+        OutlinedTextField(
+            value = password,
+            onValueChange = onValueChange,
+            singleLine = true,
+            shape = MaterialTheme.shapes.large,
+            label = { Text(text= "Digitez votre mot de passe") },
+            colors = OutlinedTextFieldDefaults.colors(
+                focusedContainerColor = MaterialTheme.colorScheme.surface,
+                unfocusedContainerColor = MaterialTheme.colorScheme.surface,
+                disabledContainerColor = MaterialTheme.colorScheme.surface,
+                focusedBorderColor = ColorPalette.Jaune,
+                unfocusedBorderColor = ColorPalette.Jaune,
+            ),
+            visualTransformation = PasswordVisualTransformation(),
+            keyboardOptions = KeyboardOptions.Default.copy(keyboardType = KeyboardType.Password)
+        )
+    }
+
+    var checked by remember { mutableStateOf(false) }
+    Row(horizontalArrangement = Arrangement.Center, verticalAlignment = Alignment.CenterVertically){
+        Checkbox(
+            checked = checked,
+            onCheckedChange = { checked = it },
+            colors = CheckboxDefaults.colors(MaterialTheme.colorScheme.primary)
+        )
+        Text(
+            text = "J'ai lu et j'accepte les conditions d'utilisation et la politique de confidentialité",
+            modifier = modifier,
+            textAlign = TextAlign.Center,
+            style= TextStyle(fontSize = TextUnit(14f, TextUnitType.Sp))
+        )
+    }
+
+    Button(
+        colors = ButtonDefaults.buttonColors(containerColor = ColorPalette.Jaune),
+        onClick = { onNextButtonClicked() }
+    ) {
+        Text(
+            text = "Creer Compte",
+            fontSize = 18.sp
+        )
+    }
+}
